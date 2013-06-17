2d9d94af79d84d1223ae3d094691b9cd1fbba4cf
Nora Spencer <nspencer@fullcoll.edu>
2013-06-16 17:28:46 -0700

    Nora's Homework [Red, 98, 4, 1/5]

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Spiral.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Spiral.java
@@ -1,6 +1,5 @@
 package org.teachingkidsprogramming.recipes;
 
-
 public class Spiral
 {
   public static void main(String[] args)
@@ -9,10 +8,10 @@ public class Spiral
     //    Make the tortoise go as fast as possible --#4
     //    Add Blue Violet to the Color Wheel --#6
     //    Add Violet to the Color Wheel --#8
-    //    Add Purple to the Color Wheel --#9
-    //    Do the following 75 times --#3
+    //    Add Red to the Color Wheel --#9
+    //    Do the following 98 times --#3
     //         Change the color of the line the tortoise draws the next color on the Color Wheel --#7
-    //         Move the tortoise 5 times the current line number you are drawing --#5
-    //         Turn the tortoise 1/3 of 360 degrees to the right --#2
+    //         Move the tortoise 4 times the current line number you are drawing --#5
+    //         Turn the tortoise 1/5 of 360 degrees to the right --#2
   }
 }
