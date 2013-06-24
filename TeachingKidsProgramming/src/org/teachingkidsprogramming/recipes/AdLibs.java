6af408c63635e4245cd02a6c36486f2f8b2296f1
Nora Spencer <nspencer@fullcoll.edu>
2013-06-24 11:25:23 -0700

    parser added with rtf

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
@@ -1,21 +1,18 @@
 package org.teachingkidsprogramming.recipes;
 
 import org.teachingextensions.simpleparser.Parser;
+import org.teachingextensions.utils.Viewer;
 import org.teachingextensions.windows.MessageBox;
 
 public class AdLibs
 {
   public static void main(String[] args)
   {
-    //create a model to hold the words
     Words words = new Words();
     words.adverb = MessageBox.askForTextInput("enter an adverb");
     words.edVerb = MessageBox.askForTextInput("enter a verb ending in -ed");
     words.bodyPart = MessageBox.askForTextInput("enter a body part");
-    String currentStory = Parser.parse("Today I woke {adverb}. Then I {edVerb} my {bodyPart}.", words);
-    // currentStory = currentStory + " I woke " + adverb + ". ";
-    //currentStory = currentStory + " Then I " + edVerb + " ";
-    // currentStory = currentStory + " my " + bodyPart + ". ";
-    MessageBox.showMessage(currentStory);
+    String currentStory = Parser.parseRtfFile("view.rtf", words);
+    Viewer.displayRtfFile(currentStory);
   }
 }
